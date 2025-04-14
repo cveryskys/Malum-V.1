83aff02402c6938062a8dcf9c6721c1b48c26c86
ayden <150983776+cveryskys@users.noreply.github.com>
2025-04-14 13:26:34 -0400

    Update server.js

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 
-const postMessage = require("./routes/postMessage");
+const postMessage = require("../routes/postMessage");
 app.use("/post-message", postMessage);
 
 const client = new Client({
